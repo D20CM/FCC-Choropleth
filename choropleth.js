4cb9835e9dcd5cec24ac5ec10eb6d25c83bc2c40
D20CM <scottbrown500@hotmail.com>
2021-10-08 23:52:30 +0100

    display map

--- choropleth.js
+++ choropleth.js
@@ -1,13 +1,15 @@
 let choropleth = function(){
+    
     Promise.all([fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"), fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json")])
     
+
     // .then(responses => Promise.all(responses.map(response => response.json)))
     .then(function (responses) {
     return Promise.all(responses.map(function (response) {
 		return response.json();
 	}))
 })
-    .then(data =>{
+    .then(data => {
   
         
 
@@ -21,16 +23,37 @@ let choropleth = function(){
         const h = 900;
         const w = 1200;
         const padding = 0;
+        let projection = d3.geoMercator();  
+        const path = d3.geoPath();
+                  
 
-        let maparea = d3.select("appContainer")
+        let maparea = d3.select("#appContainer")
                         .append("svg")
                         .attr("width", w)
                         .attr("height", h)
-        
-        maparea.append("path")
+                        .attr("fill", "blue")
+
+       maparea
+        .selectAll("path") 
+        .data(topojson.feature(usMap, usMap.objects.counties).features) 
+        .enter()
+        .append("path")
+        .attr("d", path) 
+        // .attr("transform", "scale(0.82, 0.62)")
+        // .attr("class", "county");
 
 
+        // console.log(path)
 
+        // maparea.selectAll("rect")
+        // .data(usMap.objects.counties)
+        // .enter()
+        // .append("rect")
+        // .attr("x", (d,i) => { return 10*i})
+        // .attr("y", (d,i) => 10+i)
+        // .attr("width", 10)
+        // .attr("height", 10)
+        // .attr("fill", "green")
 
 
 
